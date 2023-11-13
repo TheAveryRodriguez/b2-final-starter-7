298ad3b20a7efcc352cd7941c2718c4ab7934bc7
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-11-13 16:01:12 -0600

    US 7 model test

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe InvoiceItem, type: :model do
   describe "validations" do
@@ -14,17 +14,17 @@ RSpec.describe InvoiceItem, type: :model do
   end
 
   describe "class methods" do
-    before(:each) do
-      @m1 = Merchant.create!(name: 'Merchant 1')
-      @c1 = Customer.create!(first_name: 'Bilbo', last_name: 'Baggins')
-      @c2 = Customer.create!(first_name: 'Frodo', last_name: 'Baggins')
-      @c3 = Customer.create!(first_name: 'Samwise', last_name: 'Gamgee')
-      @c4 = Customer.create!(first_name: 'Aragorn', last_name: 'Elessar')
-      @c5 = Customer.create!(first_name: 'Arwen', last_name: 'Undomiel')
-      @c6 = Customer.create!(first_name: 'Legolas', last_name: 'Greenleaf')
-      @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: @m1.id)
-      @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8, merchant_id: @m1.id)
-      @item_3 = Item.create!(name: 'Brush', description: 'This takes out tangles', unit_price: 5, merchant_id: @m1.id)
+    it "incomplete_invoices" do
+      @m1 = Merchant.create!(name: "Merchant 1")
+      @c1 = Customer.create!(first_name: "Bilbo", last_name: "Baggins")
+      @c2 = Customer.create!(first_name: "Frodo", last_name: "Baggins")
+      @c3 = Customer.create!(first_name: "Samwise", last_name: "Gamgee")
+      @c4 = Customer.create!(first_name: "Aragorn", last_name: "Elessar")
+      @c5 = Customer.create!(first_name: "Arwen", last_name: "Undomiel")
+      @c6 = Customer.create!(first_name: "Legolas", last_name: "Greenleaf")
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @m1.id)
+      @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @m1.id)
+      @item_3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: @m1.id)
       @i1 = Invoice.create!(customer_id: @c1.id, status: 2)
       @i2 = Invoice.create!(customer_id: @c1.id, status: 2)
       @i3 = Invoice.create!(customer_id: @c2.id, status: 2)
@@ -34,9 +34,29 @@ RSpec.describe InvoiceItem, type: :model do
       @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
       @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
       @ii_4 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 1)
-    end
-    it 'incomplete_invoices' do
+
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
+    end
+  end
+
+  describe "instance method" do
+    it "discounted_af" do
+      @merchant1 = Merchant.create!(name: "Hair Care")
+
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+
+      @customer_1 = Customer.create!(first_name: "Joey", last_name: "Smith")
+
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
+      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 12, unit_price: 6, status: 1)
+
+      @bulk_discount_1 = BulkDiscount.create!(quantity_threshold: 10, percent_discount: 20, merchant_id: @merchant1.id)
+      @bulk_discount_2 = BulkDiscount.create!(quantity_threshold: 20, percent_discount: 30, merchant_id: @merchant1.id)
+
+      expect(@ii_11.discounted_af).to eq(@bulk_discount_1)
     end
   end
 end
